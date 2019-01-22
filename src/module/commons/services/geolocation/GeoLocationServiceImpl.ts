1409140cab7679d4aadfdd9091fe96393f700dbc
Juan Carlos Cancela <jccancela@aetion.com>
2019-01-22 01:55:28 -0300

    optimize imports

--- src/module/commons/services/geolocation/GeoLocationServiceImpl.ts
+++ src/module/commons/services/geolocation/GeoLocationServiceImpl.ts
@@ -1,9 +1,6 @@
 import GeoLocationService from './GeoLocationService';
 import Commandable from '../../../../utils/command/Commandable';
-import User from '../user/models/User/User';
 import GeoLocation from '../../models/GeoLocation/GeoLocation';
-import Command from '../../../../utils/command/Command';
-import CommandImpl from '../../../../utils/command/CommandImpl';
 import Modules from '../../../../utils/modules/Modules';
 import AbstractBaseService from '../../utils/services/AbstractBaseService';
 
