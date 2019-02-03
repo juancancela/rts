a03b7077cfe90fa13527c8ad286cba18aec72808
Juan Carlos Cancela <jccancela@aetion.com>
2019-02-03 16:48:23 -0200

    docs

--- src/utils/filter/FilterOperationType.ts
+++ src/utils/filter/FilterOperationType.ts
@@ -1,5 +1,7 @@
 /**
- * @description FilterOperationType
+ * @description List of operations that are capable to apply a transformation on a
+ * response. That includes filters of data based on provided values, as well as sorting
+ * and pagination.
  * @author Juan Carlos Cancela <cancela.juancarlos@gmail.com>
  */
 enum FilterOperationType {
